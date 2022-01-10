a1e3bcb1809aed1e1e73df244f1f974c63680aac
yyds <yyds@126.com>
2022-01-10 16:34:00 +0800

    nnn

--- lodash/gitxsfh-lodash.js
+++ lodash/gitxsfh-lodash.js
@@ -1,6 +1,31 @@
 var gitxsfh = {
   chunk: function (array, size) {
+    let a = []
+    let result = []
+    let total = 0
+    while (total < array.length) {
+      let every = 0
+      while (every < size) {
+        if (array[total]) {
+          a.push(array[total])
+          total++
+        }
+        every++
+      }
+      result.push(a)
+      a = []
+    }
+    return result
+  },
 
-  }
-
+  compact: function (array) {
+    let result = []
+    let long = array.length
+    for (let i = 0; i < long; i++) {
+      if (array[i]) {
+        result.push(array[i])
+      }
+    }
+    return result
+  },
 }
